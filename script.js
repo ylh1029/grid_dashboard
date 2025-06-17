eaa734b9cc0585ecbd5099c76108ef4b899ed53e
Lily H <lhirasaw@usc.edu>
2025-06-17 20:53:05 +0900

    Implemented javascript to make the page more interactive by adding clickable elements. CSS made the page responsive to the side of the page.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let control = document.querySelector(".control");
 let sidebar = document.querySelector("#sidebar");
+let favourite = document.querySelectorAll(".star");
+let closeButton = document.querySelector(".icon-close");
+let userInput = document.querySelector(".user-input");
+let sharePopup = document.querySelector(".share-popup");
+let openButton = document.querySelector("#new");
+let submitButton = document.querySelector(".submitPost");
+let shareButton = document.querySelector("#sharePage");
+let closeShare = document.querySelector(".closeShare");
+let shareIcon = document.querySelectorAll(".icons");
+let copyBtn = document.querySelector(".copyBtn");
 
 control.addEventListener("click", () => {
     if(control.classList.value === "control"){
@@ -16,3 +26,50 @@ control.addEventListener("click", () => {
         sidebar.classList.remove("active");
     }
 });
+
+favourite.forEach(item => {
+    item.addEventListener("click", () => {
+        if(item.classList.value === "star"){
+            item.classList.add("active");
+        }
+
+        else{
+            item.classList.remove("active");
+        }
+    })
+});
+
+closeButton.addEventListener("click", () => {
+    userInput.classList.remove("open");
+});
+
+openButton.addEventListener("click", () => {
+    userInput.classList.add("open");
+});
+
+submitButton.addEventListener("click", () => {
+    userInput.classList.remove("open");
+});
+
+shareButton.addEventListener("click", () => {
+    sharePopup.classList.add("open");
+});
+
+closeShare.addEventListener("click", () => {
+    sharePopup.classList.remove("open");
+})
+
+shareIcon.forEach(item => {
+    item.addEventListener("click", () =>{
+        sharePopup.classList.remove("open");
+    }); 
+});
+
+copyBtn.addEventListener("click", () => {
+    sharePopup.classList.remove("open");
+})
+
+
+
+
+
